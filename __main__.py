25d7f9356d1eefe0b0e9e0594705e6f36e7f9077
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2018-06-04 22:53:15 -0400

    power button and slider

--- __main__.py
+++ __main__.py
@@ -1,9 +1,11 @@
 import sys
 from tkinter import *
 from tkinter import ttk
-
+from throttle import throttle
+from debounce import debounce
 from config import CONFIG
 from light_controller import LightController
+from threading import Thread
 
 
 class App:
@@ -11,24 +13,63 @@ class App:
     def __init__(self, config):
         self._config = config
         self.window = Tk()
-        self._lights = LightController(self._config['lights'])
         self.window.title('Lighting Dashboard')
+        self._lights = LightController(self._config['lights'])
+        self._lights_on = False
         self._light_slider = ttk.Scale(self.window, variable=self._lights.brightness,
-                                   command=self._lights.set_all_lights_brightness,
-                                   orient=HORIZONTAL, from_=0, to=254, len=600)
-        self._light_slider.grid(row=0, column=1, sticky=W+E)
+                                       command=self.slider_change,
+                                       orient=HORIZONTAL, from_=0, to=254, len=600)
+
+        self._power_button = ttk.Button(
+            self.window, text="OFF", command=self.toggle_all_lights)
+
+        self._power_button.grid(row=1, column=1, rowspan=3, padx=(50, 50),
+                                pady=(50, 50), sticky=N + S + W + E)
+        self._light_slider.grid(
+            row=0, column=1, padx=(100, 100), pady=(50, 50))
         self.window.geometry('800x480')
 
     def main(self):
         self.init_light_controller()
         self.init_window()
 
+    @throttle(seconds=3)
+    def toggle_all_lights(self):
+        on = not self._lights_on
+        self._lights.set_all_lights_on(on)
+        self._lights_on = on
+        button_text = 'OFF' if on else 'ON'
+        self._power_button['text'] = button_text
+        self.sync_lights()
+        self.sync_window()
+
     def init_light_controller(self):
-        lights = self._lights.get_all_lights()
-        print('Initialized light controller')
+        self.sync_lights()
 
     def init_window(self):
+        self.sync_window()
         self.window.mainloop()
+
+    def sync_lights(self):
+        self._lights_on = self._lights.are_all_lights_on()
+
+    def sync_window(self):
+        button_text = 'OFF' if self._lights_on else 'ON'
+        self._power_button['text'] = button_text
+        brightness = 254 if self._lights_on else 0
+        if (brightness > 0 or brightness < 254) or self._lights.brightness == brightness:
+            return
+        if brightness == 0:
+            self._lights.set_all_lights_on(False)
+
+    @debounce(0.5)
+    def slider_change(self, brightness):
+        print(f'Brightness:{brightness}')
+        thread = Thread(
+            target=self._lights.set_all_lights_brightness, args=(brightness,))
+        on = self._lights.set_all_lights_brightness(brightness)
+        self._power_button['text'] = 'OFF' if on else 'ON'
+        thread.start()
 
     def exit(self, reason):
         print(f'Exiting app because: {reason}')
--- light_controller.py
+++ light_controller.py
@@ -12,21 +12,28 @@ class LightController:
         self._lights = self.bridge.get_light_objects('id')
         self.brightness = tk.IntVar
 
-    def set_all_lights_on(self):
-        new_light_state = {'transitiontime': 30, 'on': True, 'bri': 254, 'sat': 0}
+    def set_all_lights_on(self, on=True):
+        new_light_state = {'transitiontime': 10, 'on': on, 'bri': 254, 'sat': 0}
         self.bridge.set_light(self._lights, new_light_state)
 
-    def set_all_lights_off(self):
-        new_light_state = {'transitiontime': 30, 'on': False, 'bri': 254, 'sat': 0}
-        self.bridge.set_light(self._lights, new_light_state)
-
-    @debounce(0.5)
     def set_all_lights_brightness(self, brightness):
         bri = round(int(float(brightness)))
+        if bri < 1:
+            self.set_all_lights_on(False)
+            self.brightness = 0
+            return False
+
         new_light_state = {'transitiontime': 0, 'on': True, 'bri': bri}
         self.bridge.set_light(self._lights, new_light_state)
         self.brightness = brightness
-        print(f'brightness: {brightness}')
+        return True
 
     def get_all_lights(self):
         return self._lights
+
+    def are_all_lights_on(self):
+        lights_on = True
+        for light in self.bridge.get_light_objects():
+            if not light.on:
+                return False
+        return lights_on
